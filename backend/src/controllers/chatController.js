5737aef2192e222f39a47c81f16c6ede6b4158d3
FLYBENJIFLY\benja <benjivds@gmail.com>
2025-11-05 16:33:15 -0600

    return console.error instead of console.log in error message

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -12,7 +12,7 @@ export async function getStreamToken(req, res) {
       userImage: req.user.image,
     });
   } catch (error) {
-    console.log("Error in getStreamToken controller:", error.message);
+    console.error("Error in getStreamToken controller:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
