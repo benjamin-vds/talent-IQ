11ac1e8e5c296746f975cbcbd75bb3985737a6bf
FLYBENJIFLY\benja <benjivds@gmail.com>
2025-11-05 09:45:59 -0600

    changed env.js validating missing variables

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,6 +2,12 @@ import dotenv from "dotenv";
 // reading the enviroment files
 dotenv.config();
 
+const requiredEnvVars = ['PORT','DB_URL'];
+const missing = requiredEnvVars.filter((key)=> !process.env[key]);
+
+if(missing.length > 0){
+    throw new Error(`Missing required environment variables: ${missing.join(',')}`)
+}
 
 //Setting the enviroment variables
 export const ENV = {
