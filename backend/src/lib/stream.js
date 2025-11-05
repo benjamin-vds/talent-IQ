035bd7446dddd6a11fae2ed70f8ed523fed5f47e
FLYBENJIFLY\benja <benjivds@gmail.com>
2025-11-05 14:53:03 -0600

    changing key of  stream secret

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -2,7 +2,7 @@ import {StreamChat} from "stream-chat";
 import { ENV } from "./env.js";
 
 const apiKey =ENV.STREAM_API_KEY;
-const apiSecret = ENV.STREAM_API_SECRETY;
+const apiSecret = ENV.STREAM_API_SECRET;
 
 export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
 
