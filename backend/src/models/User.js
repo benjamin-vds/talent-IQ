d2ed96edc1b0e644572fc35548d7b8b8aa0789d4
FLYBENJIFLY\benja <benjivds@gmail.com>
2025-11-05 15:10:52 -0600

    missing clerkID

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
   { timestamps: true }  // createdAt, updatedAt adding
 );
