8c058b92aa1a6cfae33a4aeb3a89cb6f7e923aa8
FLYBENJIFLY\benja <benjivds@gmail.com>
2025-11-05 11:11:33 -0600

    connecting to mongodb

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 // reading the enviroment files
 dotenv.config();
-
+// we don't check port because it will be deployed on default port
 const requiredEnvVars = ['DB_URL', 'NODE_ENV'];
+console.log('test : ', process.env.DB_URL);
+
+
 const missing = requiredEnvVars.filter((key)=> !process.env[key]);
 
 if(missing.length > 0){
